4b02b0d48a4ea07007e3dda35b838818fa28ce8a
Woraprat Rattana <worapratrattana@MillenniumFalcon-2.local>
2020-01-04 01:13:40 +0700

    [Feature] Searchbar

--- src/app/page/feeds/feeds.page.ts
+++ src/app/page/feeds/feeds.page.ts
@@ -7,17 +7,36 @@ import { PsuHospitalService } from './../../services/psu-hospital.service';
   styleUrls: ['./feeds.page.scss'],
 })
 export class FeedsPage implements OnInit {
-  locations: Array<Location> = [];
+  allLocations: Array<Location> = [];
+  filterLocations: Array<Location>  = [];
   lastPage  = false;
-  constructor(private psuHospitalService: PsuHospitalService) {}
-
-  ngOnInit() {
+  textStatus = 'สถานที่ทั้งหมด';
+  searchInput = '';
+  constructor(private psuHospitalService: PsuHospitalService) {
     this.psuHospitalService.loadLocation(0).subscribe(
       results => {
-        this.locations = results.results;
+        this.allLocations = results.results;
+        this.filterLocations = results.results;
         this.lastPage = results.lastPage;
       }
     );
   }
+
+  ngOnInit() {
+  }
+
+  inputSearch(event) {
+    this.textStatus = 'ผลการค้นหา';
+    const result = event.target.value;
+    this.searchInput = result;
+    console.log(this.searchInput);
+    this.setFilteredLocations();
+  }
+  setFilteredLocations() {
+    this.filterLocations = this.allLocations.filter((item) => {
+      return item.name.toLowerCase().indexOf(this.searchInput.toLowerCase()) > -1;
+    });
+  }
+
 
 }
