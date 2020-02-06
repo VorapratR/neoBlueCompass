b9952df22d344815db18bc79b1c4c779f55c5b4f
JAKEE INDAPANYA (จาคี อินทปัญญา) <5910110044@email.psu.ac.th>
2020-02-07 00:53:37 +0700

    text generater

--- src/app/page/process/map/map.page.ts
+++ src/app/page/process/map/map.page.ts
@@ -27,8 +27,9 @@ export class MapPage implements OnInit, OnDestroy {
   stepCount: number;
   allLocations: Array<Location> = [];
   bsub: Subscription;
-  pathResult: any = {};
-  pathCoordinations: any[][] = [[], []];
+  graph: any = {};
+  textOrder: string[] = ['เริ่มต้น'];
+  pathResults: any[][] = [[], []];
   constructor(
     private location: Location,
     private router: Router,
@@ -56,47 +57,61 @@ export class MapPage implements OnInit, OnDestroy {
     const nodeDijkstra = require('node-dijkstra');
     this.bsub = this.psuHospitalService.loadLocation().subscribe(
       data => {
-        this.pathResult = {};
+        this.graph = {};
         this.allLocations = data.locations;
         const results = {};
         Object.keys(this.allLocations).map(
           elem => {
-            const pathId = this.allLocations[elem]['id'];
-            results[pathId] = this.allLocations[elem]['neighbor'];
+            const pathId = this.allLocations[elem].id;
+            results[pathId] = this.allLocations[elem].neighbor;
           }
         );
         const route = new nodeDijkstra(results);
-        this.pathResult = route.path(this.idStart, this.idGoal, { cost: true });
-        // console.log(this.pathResult);
+        this.graph = route.path(this.idStart, this.idGoal, { cost: true });
+        // console.log(this.graph);
         // console.log(paths);
         // tslint:disable-next-line: forin
-        // this.pathResult['path'].forEach(path => {
+        // this.graph['path'].forEach(path => {
           // if
         // });
         const drawResult = [[], []];
-        this.pathResult['path'].forEach(element => {
+        this.graph.path.forEach(element => {
           Object.keys(this.allLocations).map(
             elem => {
               let obj = {};
-              if (element === this.allLocations[elem]['id']) {
+              if (element === this.allLocations[elem].id) {
                 obj = {
-                  id: this.allLocations[elem]['id'],
-                  x: this.allLocations[elem]['x'],
-                  y: this.allLocations[elem]['y'],
+                  id: this.allLocations[elem].id,
+                  x: this.allLocations[elem].x,
+                  y: this.allLocations[elem].y,
+                  name: this.allLocations[elem].name
                 };
-                if (this.allLocations[elem]['id'].includes('baramee1')) {
+                if (this.allLocations[elem].id.includes('baramee1')) {
                   console.log(true);
                   drawResult[0].push(obj);
                 }
-                else if (this.allLocations[elem]['id'].includes('main1')) {
+                else if (this.allLocations[elem].id.includes('main1')) {
                   console.log(false);
                   drawResult[1].push(obj);
                 }
                 console.log(drawResult);
               }});
         });
-        this.pathCoordinations = drawResult;
+        this.pathResults = drawResult;
+        let newArray = [];
+        newArray = drawResult[0].concat(drawResult[1]);
+        // this.text
         // console.log('drawResult => ', drawResult);
+        // console.log('newArray => ', newArray);
+        // const arrArray = [
+        //   ['Krunal', 'Ankit'],
+        //   ['Rushabh', 'Dhaval']
+        // ];
+        // arrArray.push([
+        //   'Tejash', 'Rajesh'
+        // ]);
+        // console.log(arrArray);
+        this.generateText(newArray);
       }
     );
   }
@@ -140,18 +155,19 @@ export class MapPage implements OnInit, OnDestroy {
     this.canvas.height = img.height;
     // console.log(img.src.includes('baramee1'));
     if (img.src.includes('baramee1')) {
-      this.drawLine(this.canvas.getContext('2d'), img, this.pathCoordinations[0]);
+      this.drawLine(this.canvas.getContext('2d'), img, this.pathResults[0]);
     }
     if (img.src.includes('main1')) {
-      this.drawLine(this.canvas.getContext('2d'), img, this.pathCoordinations[1]);
+      this.drawLine(this.canvas.getContext('2d'), img, this.pathResults[1]);
     }
     // this.drawLine(this.canvas.getContext('2d'), img);
     const dataURL = this.canvas.toDataURL('image/png');
     return dataURL.replace(/^data:image\/(png|jpg);base64,/, '');
   }
 
+  // nodePath เป็น array ของ pathResults ที่ใช้ในการสร้าง map
   drawLine(ctx: any, img: HTMLImageElement, nodePath: Object[]) {
-    // console.log('in drawline', this.pathCoordinations);
+    // console.log('in drawline', this.pathResults);
     ctx.drawImage(img, 0, 0);
     ctx.beginPath();
     // ctx.arc(100, 100, 50, 0, 2 * Math.PI, false);
@@ -160,16 +176,16 @@ export class MapPage implements OnInit, OnDestroy {
     ctx.lineWidth = 15;
     for (let i = 0; i < nodePath.length; i++) {
       if (i === 0) {
-        ctx.moveTo(nodePath[i + 1]['x'], nodePath[i + 1]['y']);
+        ctx.moveTo(nodePath[i + 1].x, nodePath[i + 1].y);
         ctx.fillStyle = '#DC143C';
-        ctx.fillRect(nodePath[i + 1]['x'] - 10, nodePath[i + 1]['y'], 20, 20);
+        ctx.fillRect(nodePath[i + 1].x - 10, nodePath[i + 1].y, 20, 20);
       } else if (nodePath[i + 1] == null) {
         // console.log("in case");
-        ctx.moveTo(nodePath[i]['x'], nodePath[i]['y']);
+        ctx.moveTo(nodePath[i].x, nodePath[i].y);
         ctx.fillStyle = '#DC143C';
-        ctx.fillRect(nodePath[i]['x'] - 10, nodePath[i]['y'] - 20, 20, 20);
+        ctx.fillRect(nodePath[i].x - 10, nodePath[i].y - 20, 20, 20);
       } else if (nodePath[i + 1] != null) {
-        ctx.lineTo(nodePath[i + 1]['x'], nodePath[i + 1]['y']);
+        ctx.lineTo(nodePath[i + 1].x, nodePath[i + 1].y);
         // ctx.fillStyle = '#00BFFF';
       }
     }
@@ -217,6 +233,41 @@ export class MapPage implements OnInit, OnDestroy {
       });
     }
 
+  }
+
+  CalculateCrossProduct(A: any, B: any, C: any): string {
+    const bx = B.x - A.x;
+    const by = B.y - A.y;
+    const cx = C.x - A.x;
+    const cy = C.y - A.y;
+    const crossProduct = bx * cy - by * cx;
+    // console.log('====> b in cp', B);
+    if (crossProduct > 0) {
+      // console.log(`เลี้ยวขวา${B.name}`);
+      return `เลี้ยวขวาที่${B.name},`;
+    } else if (crossProduct < 0) {
+      // console.log(`เลี้ยวซ้าย${B.name}`);
+      return `เลี้ยวซ้ายที่${B.name},`;
+    }
+    return '';
+  }
+
+  generateText(data: any) {
+    console.log('data in generateText', data);
+    let command = 'เดินตรงไปแล้ว';
+    for (let i = 0; i < data.length - 2; i++) {
+      const buffer = this.CalculateCrossProduct(data[i], data[i + 1], data[i + 2]);
+      if (buffer !== '') {
+        command += buffer;
+        command += 'จากนั้นมุ่งหน้าต่อไปแล้ว';
+      }
+    }
+    this.textOrder = command.split(',');
+    this.textOrder.unshift('เริ่มต้น');
+    this.textOrder.pop();
+    this.textOrder.push('เดินตรงไป');
+    this.textOrder.push('ถึงจุดหมาย');
+    console.log(this.textOrder);
   }
 
   backBeforePage() {
