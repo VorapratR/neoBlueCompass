ecf316f29d0ca50e0c35f42bc6cf2f50626b17bd
Woraprat Rattana <worapratrattana@MillenniumFalcon-2.local>
2020-03-05 00:27:31 +0700

    [Featurn] Connent firebase test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBi7HmrnSVwBSfzu23AUbl81AdAjdwOW5A',
+    authDomain: 'fir-ionic-248cd.firebaseapp.com',
+    databaseURL: 'https://fir-ionic-248cd.firebaseio.com',
+    projectId: 'fir-ionic-248cd',
+    storageBucket: 'fir-ionic-248cd.appspot.com',
+    messagingSenderId: '663579322175',
+    appId: '1:663579322175:web:846543a8c89932e931d58a',
+    measurementId: 'G-W5X2V8TJ60'
+  }
 };
 
 /*
